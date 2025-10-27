a6527db364420c2293e5d50e4ddc653f9df3b0a5
Lora-Line <loralinef@live.fr>
2025-10-27 14:57:49 +0100

    added colorsets and custom fonts

---
+++ Craftea/Fonts/Fonts.swift
@@ -0,0 +1,33 @@
+//
+//  Fonts.swift
+//
+//
+//  Created by apprenant75 on 27/10/2025.
+//
+
+import SwiftUI
+
+
+extension View {
+    func mainTitle() -> some View {
+        return self.font(.custom("Manrope-Bold", size: 28))
+    }
+    func secondaryTitle() -> some View {
+        return self.font(.custom("Manrope-Bold", size: 21))
+    }
+    func tertiaryTitle() -> some View {
+        return self.font(.custom("Manrope-Regular", size: 16))
+    }
+    func mainText() -> some View {
+        return self.font(.custom("Inter_24pt-Regular", size: 16))
+    }
+    func secondaryText() -> some View {
+        return self.font(.custom("Inter_24pt-Regular", size: 14))
+    }
+    func mainTextBold() -> some View {
+        return self.font(.custom("Inter_24pt-SemiBold", size: 16))
+    }
+}
+
+
+
