522f6a677c5f7f812b3d69f31b10e1cd52aa9ab0
Lora-Line <loralinef@live.fr>
2025-10-27 14:21:40 +0100

    creation of models for User Hobby and Equipment

---
+++ Craftea/Model/User.swift
@@ -0,0 +1,40 @@
+//
+//  User.swift
+//  Craftea
+//
+//  Created by apprenant75 on 27/10/2025.
+//
+
+import Foundation
+import Observation
+
+@Observable
+class User: Identifiable {
+    let id = UUID()
+    var name: String
+    var surname: String
+    var mail: String
+    var pseudo: String
+    var password: String
+    var location: String?
+    var score: Double
+    var favoriteEquipment: [Equipment?]
+    var favoritesHobby: [Hobbies?]
+    var equipment: [Equipment?]
+    //TO-DO
+    //prendre de questionnaire en compte
+    
+    init(name: String, surname: String, mail: String, pseudo: String, password: String, location: String? = nil, score: Double = 5, favoriteEquipment: [Equipment?], favoritesHobby: [Hobbies?], equipment: [Equipment?]) {
+        self.name = name
+        self.surname = surname
+        self.mail = mail
+        self.pseudo = pseudo
+        self.password = password
+        self.location = location
+        self.score = score
+        self.favoriteEquipment = favoriteEquipment
+        self.favoritesHobby = favoritesHobby
+        self.equipment = equipment
+    }
+}
+
